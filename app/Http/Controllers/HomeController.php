132d962576c4336b537a73355bd45c98e42d4495
Ahimbisibwe Roland <rolandmbasa@gmail.com>
2020-02-11 00:53:23 +0300

    add auth

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\Auth;
-
 class HomeController extends Controller
 {
     /**
@@ -17,10 +15,10 @@ class HomeController extends Controller
     /**
      * Show the application dashboard.
      *
-     * @return \Illuminate\Contracts\Support\Renderable
+     * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        return view('home')->with(['user' => Auth::user()]);
+        return view('home');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,10 @@
 <?php
 
-use App\Http\Controllers\Auth\APILoginController;
 
 Route::middleware('auth:airlock')
     ->prefix('v1')
     ->group(base_path('routes/v1/all.php'));
 
 Route::post('v1/auth/register', 'Auth\UserRegistrationController@register');
-Route::post('v1/auth/token', Auth\IssueOAuthTokensController::class);
 
-Route::post('v1/auth/login', [APILoginController::class, 'login'])->middleware(['web']);
+Route::post('v1/auth/token', Auth\IssueOAuthTokensController::class);
